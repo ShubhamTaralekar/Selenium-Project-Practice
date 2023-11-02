3a94b18de5b13966ebd75d97bd3f0b66aead4c0e
Admin <Admin@DESKTOP-9PKBUHS>
2023-11-02 16:37:52 -0700

    FirstCommit

---
+++ src/main/java/com/test/AmazoneTest.java
@@ -0,0 +1,59 @@
+package com.test;
+
+
+
+import java.time.Duration;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class AmazoneTest {
+
+	public static void main(String[] args) throws InterruptedException  {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver= new ChromeDriver();
+		new WebDriverWait(driver, Duration.ofSeconds(10));
+	
+		driver.manage().window().maximize();
+		driver.get("https://www.amazon.in/");
+	
+
+		WebElement searchBox =driver.findElement(By.id("twotabsearchtextbox"));
+		searchBox.sendKeys("iphone15 Pro Max (256 GB)");
+		WebElement searchbtn =driver.findElement(By.xpath("//input[@type='submit']"));
+		searchbtn.click();
+		
+		WebElement iphone=driver.findElement(By.linkText("Apple iPhone 15 Pro Max (256 GB) - Blue Titanium"));
+		iphone.click();
+		
+	
+	//searchBox0.submit();
+		//
+		 Set<String> newWindow=driver.getWindowHandles();//parentId,childId,subchidId
+		 
+		 Iterator<String> it= newWindow.iterator();
+		 String parentId=(String)it.next();
+		 String childId= (String) it.next();
+		 driver.switchTo().window(childId);
+		
+		
+		WebElement buybtn=driver.findElement(By.xpath("//input[@id='buy-now-button']"));
+		buybtn.click();
+		
+		driver.switchTo().window(parentId);
+	
+	
+
+		
+
+	}
+
+}
